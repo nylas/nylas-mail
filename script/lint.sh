57fc6d61e5e5bc113881d340dfe6ba5206b46d3a
Evan Morikawa <evan@evanmorikawa.com>
2015-05-21 14:50:40 -0700

    update linter to look at all less files
    Test Plan: script/grunt lint
    
    Reviewers: bengotow
    
    Reviewed By: bengotow
    
    Differential Revision: https://phab.nylas.com/D1549

--- script/lint.sh
+++ script/lint.sh
@@ -4,4 +4,4 @@ GO=0
 if [ ${1##*.} == "coffee" ]; then GO=1; fi
 if [ ${1##*.} == "cjsx" ]; then GO=1; fi
 if [ $GO != "1" ]; then exit 0; fi
-script/grunt coffeelint:target --target "$*" || true
+script/grunt lint:target --target "$*" || true
