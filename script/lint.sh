eb3672965125fb77575aa28b220f7fc64b2dff72
Rob McQueen <rob@nylas.com>
2015-07-15 13:18:10 -0700

    Separate notifications if less than five
    Summary: If less than 5 new unread notifications, then show notification for each new message. Each message is separated by 1 second
    
    Test Plan: Added test to make sure Notification is called multiple times
    
    Reviewers: bengotow
    
    Reviewed By: bengotow
    
    Maniphest Tasks: T1295
    
    Differential Revision: https://phab.nylas.com/D1744

--- script/lint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# This is invoked by 'arc lint', see .arcconfig.
-GO=0
-if [ ${1##*.} == "coffee" ]; then GO=1; fi
-if [ ${1##*.} == "cjsx" ]; then GO=1; fi
-if [ $GO != "1" ]; then exit 0; fi
-script/grunt lint:target --target "$*" || true
