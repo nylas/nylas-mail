e53b481a25601967869aa08f9392ec7f0feec850
Jackie Luo <jacquelineluo95@gmail.com>
2016-02-09 13:27:41 -0800

    fix(composer-emojis): Add spec and allow adjacent emojis
    Summary:
    Adds tests to the emoji picker.
    
    The emoji picker should also now be able to add emojis consecutively (without spaces).
    
    Test Plan: Tests included in diff.
    
    Reviewers: evan, bengotow
    
    Differential Revision: https://phab.nylas.com/D2551

--- internal_packages/composer-emojis/lib/emojis-composer-extension.jsx
+++ internal_packages/composer-emojis/lib/emojis-composer-extension.jsx
@@ -14,7 +14,7 @@ class EmojisComposerExtension extends ContenteditableExtension {
         const offset = sel.anchorOffset;
         if (!DOMUtils.closest(sel.anchorNode, "n1-emoji-autocomplete")) {
           editor.select(sel.anchorNode,
-                        sel.anchorOffset - triggerWord.length,
+                        sel.anchorOffset - triggerWord.length - 1,
                         sel.focusNode,
                         sel.focusOffset).wrapSelection("n1-emoji-autocomplete");
           editor.select(sel.anchorNode,
@@ -26,9 +26,9 @@ class EmojisComposerExtension extends ContenteditableExtension {
         if (DOMUtils.closest(sel.anchorNode, "n1-emoji-autocomplete")) {
           editor.unwrapNodeAndSelectAll(DOMUtils.closest(sel.anchorNode, "n1-emoji-autocomplete"));
           editor.select(sel.anchorNode,
-                        sel.anchorOffset + triggerWord.length,
+                        sel.anchorOffset + triggerWord.length + 1,
                         sel.focusNode,
-                        sel.focusOffset + triggerWord.length);
+                        sel.focusOffset + triggerWord.length + 1);
         }
       }
     } else {
@@ -49,9 +49,8 @@ class EmojisComposerExtension extends ContenteditableExtension {
       if (emojiOptions.length > 0 && !toolbarState.dragging && !toolbarState.doubleDown) {
         const locationRefNode = DOMUtils.closest(sel.anchorNode,
                                                  "n1-emoji-autocomplete");
-        const emojiNameNode = DOMUtils.closest(sel.anchorNode,
-                                               "n1-emoji-autocomplete");
-        const selectedEmoji = emojiNameNode.getAttribute("selectedEmoji");
+        if (!locationRefNode) return null;
+        const selectedEmoji = locationRefNode.getAttribute("selectedEmoji");
         return {
           component: EmojiPicker,
           props: {emojiOptions,
@@ -111,22 +110,22 @@ class EmojisComposerExtension extends ContenteditableExtension {
         sel.anchorNode.nodeValue &&
         sel.anchorNode.nodeValue.length > 0 &&
         sel.isCollapsed) {
-      const words = sel.anchorNode.nodeValue.substring(0, sel.anchorOffset).split(" ");
-      let lastWord = words[words.length - 1].trim();
-      if (words.length === 1 &&
-          lastWord.indexOf(" ") === -1 &&
-          lastWord.indexOf(":") === -1) {
+      const words = sel.anchorNode.nodeValue.substring(0, sel.anchorOffset);
+      let index = words.lastIndexOf(":");
+      let lastWord = "";
+      if (index !== -1 && words.lastIndexOf(" ") < index) {
+        lastWord = words.substring(index + 1, sel.anchorOffset);
+      } else {
         const {text} = EmojisComposerExtension._getTextUntilSpace(sel.anchorNode, sel.anchorOffset);
-        lastWord = text;
-      }
-      if (lastWord.length > 1 &&
-          lastWord.charAt(0) === ":" &
-          lastWord.charAt(lastWord.length - 1) !== " ") {
-        let word = lastWord.substring(1);
-        if (lastWord.charAt(lastWord.length - 1) === ":") {
-          word = word.substring(0, word.length - 1);
+        index = text.lastIndexOf(":");
+        if (index !== -1 && text.lastIndexOf(" ") < index) {
+          lastWord = text.substring(index + 1);
+        } else {
+          return {triggerWord: "", emojiOptions: []};
         }
-        return {triggerWord: lastWord, emojiOptions: EmojisComposerExtension._findMatches(word)};
+      }
+      if (lastWord.length > 0) {
+        return {triggerWord: lastWord, emojiOptions: EmojisComposerExtension._findMatches(lastWord)};
       }
       return {triggerWord: lastWord, emojiOptions: []};
     }
@@ -140,22 +139,22 @@ class EmojisComposerExtension extends ContenteditableExtension {
     if (sel.anchorNode &&
         sel.anchorNode.nodeValue &&
         sel.anchorNode.nodeValue.length > 0 &&
-        sel.isCollapsed) {
-      const words = sel.anchorNode.nodeValue.substring(0, sel.anchorOffset).split(" ");
-      let lastWord = words[words.length - 1].trim();
-      if (words.length === 1 &&
-          lastWord.indexOf(" ") === -1 &&
-          lastWord.indexOf(":") === -1) {
-        const {text, textNode} = EmojisComposerExtension._getTextUntilSpace(sel.anchorNode, sel.anchorOffset);
-        lastWord = text;
-        const offset = textNode.nodeValue.lastIndexOf(":");
-        editor.select(textNode,
-                      offset,
+          sel.isCollapsed) {
+      const words = sel.anchorNode.nodeValue.substring(0, sel.anchorOffset);
+      let index = words.lastIndexOf(":");
+      let lastWord = words.substring(index + 1, sel.anchorOffset);
+      if (index !== -1 && words.lastIndexOf(" ") < index) {
+        editor.select(sel.anchorNode,
+                      sel.anchorOffset - lastWord.length - 1,
                       sel.focusNode,
                       sel.focusOffset);
       } else {
-        editor.select(sel.anchorNode,
-                      sel.anchorOffset - lastWord.length,
+        const {text, textNode} = EmojisComposerExtension._getTextUntilSpace(sel.anchorNode, sel.anchorOffset);
+        index = text.lastIndexOf(":");
+        lastWord = text.substring(index + 1);
+        const offset = textNode.nodeValue.lastIndexOf(":");
+        editor.select(textNode,
+                      offset,
                       sel.focusNode,
                       sel.focusOffset);
       }
