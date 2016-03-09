e94dbbdca1c1c64fe3c1d500d437e293beb1173c
Jackie Luo <jacquelineluo95@gmail.com>
2016-03-08 17:32:23 -0800

    fix(theme-picker): Move launch command to activate()

--- internal_packages/theme-picker/lib/main.js
+++ internal_packages/theme-picker/lib/main.js
@@ -1,23 +1,10 @@
 /** @babel */
-import React from 'react';
-import Actions from '../../../src/flux/actions';
-
-import ThemePicker from './theme-picker';
 import ThemePickerStore from './theme-picker-store';
-
 
 export function activate() {
   ThemePickerStore.activate();
-  this.disposable = NylasEnv.commands.add("body", "window:launch-theme-picker",  () => {
-    Actions.openModal(
-       children=<ThemePicker />,
-       height=400,
-       width=250,
-    );
-  });
 }
 
 export function deactivate() {
   ThemePickerStore.deactivate();
-  this.disposable.dispose();
 }
