3c8398a9482763be6ab0b2c8452c9a159197d99c
Simon Seyer <simon.seyer@gmail.com>
2016-03-11 23:57:58 +0100

    Added button to hide phishing warning

--- internal_packages/phishing-detection/lib/main.jsx
+++ internal_packages/phishing-detection/lib/main.jsx
@@ -5,7 +5,10 @@ import {
   // A `Store` is a Flux component which contains all business logic and data
   // models to be consumed by React components to render markup.
   MessageStore,
+  Actions,
 } from 'nylas-exports';
+import {RetinaImg} from 'nylas-component-kit'
+import {PLUGIN_ID} from './phishing-detection-constants'
 
 // Notice that this file is `main.cjsx` rather than `main.coffee`. We use the
 // `.cjsx` filetype because we use the CJSX DSL to describe markup for React to
@@ -37,6 +40,14 @@ class PhishingIndicator extends React.Component {
     });
   }
 
+  _onHide = () => {
+    const {message} = this.state;
+    const metadata = message.metadataForPluginId(PLUGIN_ID) || {};
+
+    metadata.hide = true;
+    Actions.setMetadata(message, PLUGIN_ID, metadata);
+  }
+
   // A React component's `render` method returns a virtual DOM element described
   // in CJSX. `render` is deterministic: with the same input, it will always
   // render the same output. Here, the input is provided by @isPhishingAttempt.
@@ -48,15 +59,24 @@ class PhishingIndicator extends React.Component {
     // phishing attempt boils down to checking the `replyTo` attributes on
     // `Message` models from `MessageStore`.
     if (message && message.replyTo && message.replyTo.length !== 0) {
-      const from = message.from[0].email;
-      const replyTo = message.replyTo[0].email;
-      if (replyTo !== from) {
-        return (
-          <div className="phishingIndicator">
-            <b>This message looks suspicious!</b>
-            <div>{`It originates from ${from} but replies will go to ${replyTo}.`}</div>
-          </div>
-        );
+      // Don't show the phishing indicator when the user hided it before
+      const metadata = message.metadataForPluginId(PLUGIN_ID);
+      if (!metadata || metadata.hide !== true) {
+        const from = message.from[0].email;
+        const replyTo = message.replyTo[0].email;
+        if (replyTo !== from) {
+          return (
+            <div className="phishingIndicator">
+              <b>This message looks suspicious!</b>
+              <RetinaImg
+                className="x"
+                name="label-x.png"
+                mode={RetinaImg.Mode.ContentIsMask}
+                onClick={this._onHide}/>
+              <div className="description">{`It originates from ${from} but replies will go to ${replyTo}.`}</div>
+            </div>
+          );
+        }
       }
     }
 
