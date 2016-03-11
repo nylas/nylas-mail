2c723725b61bc4bbf857839667e718f172d91414
Jackie Luo <jacquelineluo95@gmail.com>
2016-03-11 11:01:33 -0800

    fix(themes): Add quick fixes for Ubuntu

--- internal_packages/theme-picker/lib/theme-picker.jsx
+++ internal_packages/theme-picker/lib/theme-picker.jsx
@@ -47,7 +47,7 @@ class ThemePicker extends React.Component {
   }
 
   _renderThemeOptions() {
-    const internalThemes = ['ui-taiga', 'ui-darkside', 'ui-dark', 'ui-light'];
+    const internalThemes = ['ui-ubuntu', 'ui-taiga', 'ui-darkside', 'ui-dark', 'ui-light'];
     const sortedThemes = [].concat(this.state.themes);
     sortedThemes.sort((a, b) => {
       return (internalThemes.indexOf(a.name) - internalThemes.indexOf(b.name)) * -1;
