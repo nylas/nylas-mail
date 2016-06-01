c6e28d3ebba5f1bbf628c0074366823b27006416
Jackie Luo <jacquelineluo95@gmail.com>
2016-06-01 11:11:51 -0700

    fix(emoji): Remove special casing for Outlook emoji

--- internal_packages/composer-emoji/lib/emoji-message-extension.jsx
+++ internal_packages/composer-emoji/lib/emoji-message-extension.jsx
@@ -18,26 +18,7 @@ class EmojiMessageExtension extends MessageViewExtension {
   static renderedMessageBodyIntoDocument({document}) {
     const emojiRegex = RegExpUtils.emojiRegex();
 
-    // special case: Find outlook-style emoji, where it's an image with an emoji alt-text.
-    // <img alt="😊" class="EmojiInsert">
-    const emojiImageTags = document.createTreeWalker(document.body, NodeFilter.SHOW_ELEMENT, {
-      acceptNode: (node) => {
-        if ((node.nodeName === 'IMG') && node.alt) {
-          return NodeFilter.FILTER_ACCEPT;
-        }
-        return NodeFilter.FILTER_SKIP;
-      },
-    });
-
-    while (emojiImageTags.nextNode()) {
-      const node = emojiImageTags.currentNode;
-      const emojiNameForAlt = emoji.which(node.alt);
-      if (emojiNameForAlt) {
-        makeIntoEmojiTag(node, emojiNameForAlt);
-      }
-    }
-
-    // general case: look for emoji in the content of text nodes
+    // Look for emoji in the content of text nodes
     const treeWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
 
     while (treeWalker.nextNode()) {
