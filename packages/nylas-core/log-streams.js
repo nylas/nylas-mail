5e0bcc5effdb487f79f68e5656fc16e4846c0338
Evan Morikawa <evan@evanmorikawa.com>
2016-07-14 18:36:21 -0700

    Quieter logging on dev

--- packages/nylas-core/log-streams.js
+++ packages/nylas-core/log-streams.js
@@ -25,9 +25,22 @@ const stdoutStream = {
 const getLogStreams = (name, env) => {
   switch (env) {
     case 'development': {
-      const prettyStdOut = new PrettyStream();
+      const prettyStdOut = new PrettyStream({
+        mode: 'pm2',
+        lessThan: 'error',
+      });
+      const prettyStdErr = new PrettyStream({
+        mode: 'pm2'
+      });
       prettyStdOut.pipe(process.stdout);
+      prettyStdErr.pipe(process.stderr);
       return [
+        {
+          type: 'raw',
+          level: 'error',
+          stream: prettyStdErr,
+          reemitErrorEvents: true,
+        },
         {
           type: 'raw',
           level: 'debug',
