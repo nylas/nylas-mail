f9f78968b15896a496624e12e4609750cd2421f5
Ben Gotow <bengotow@gmail.com>
2016-07-15 19:01:50 -0700

    Use defaultValue as a template, fix critical sync issue

--- packages/nylas-core/database-types.js
+++ packages/nylas-core/database-types.js
@@ -5,18 +5,22 @@ module.exports = {
     type: Sequelize.TEXT,
     get: function get() {
       const val = this.getDataValue(fieldName);
-      if (!val) { return defaultValue }
+      if (!val) {
+        return defaultValue ? Object.assign({}, defaultValue) : null;
+      }
       return JSON.parse(val);
     },
     set: function set(val) {
       this.setDataValue(fieldName, JSON.stringify(val));
     },
   }),
-  JSONARRAYType: (fieldName, {defaultValue = []} = {}) => ({
+  JSONARRAYType: (fieldName) => ({
     type: Sequelize.TEXT,
     get: function get() {
       const val = this.getDataValue(fieldName);
-      if (!val) { return defaultValue }
+      if (!val) {
+        return [];
+      }
       return JSON.parse(val);
     },
     set: function set(val) {
