7618cc543a2b73a0f83897873e66cade219a4390
Halla Moore <halla@nylas.com>
2016-11-22 15:03:10 -0800

    [local-sync] Check for missing Category roles by localized display names
    Sometimes imap mailboxes aren't properly flagged with their roles, so we
    check the display names against known variations to see if we can find any
    missing roles.

---
+++ packages/local-sync/spec/sync/mock-database.js
@@ -0,0 +1,55 @@
+const {DatabaseConnector} = require('nylas-core');
+
+/*
+ * Mocks out various Model and Instance methods to prevent actually saving data
+ * to the sequelize database. Note that with the current implementation, only
+ * instances created with Model.build() are mocked out.
+ *
+ * Currently mocks out the following:
+ *   Model
+ *     .build()
+ *     .findAll()
+ *   Instance
+ *     .destroy()
+ *     .save()
+ *
+ */
+function mockDatabase() {
+  return DatabaseConnector.forAccount(-1).then((db) => {
+    const data = {};
+    for (const modelName of Object.keys(db.sequelize.models)) {
+      const model = db.sequelize.models[modelName];
+      data[modelName] = {};
+
+      spyOn(model, 'findAll').and.callFake(() => {
+        return Promise.resolve(
+          Object.keys(data[modelName]).map(key => data[modelName][key])
+        );
+      });
+
+      const origBuild = model.build;
+      spyOn(model, 'build').and.callFake((...args) => {
+        const instance = origBuild.apply(model, args);
+
+        spyOn(instance, 'save').and.callFake(() => {
+          if (instance.id == null) {
+            const sortedIds = Object.keys(data[modelName]).sort();
+            const len = sortedIds.length;
+            instance.id = len ? +sortedIds[len - 1] + 1 : 0;
+          }
+          data[modelName][instance.id] = instance;
+        });
+
+        spyOn(instance, 'destroy').and.callFake(() => {
+          delete data[modelName][instance.id]
+        });
+
+        return instance;
+      })
+    }
+
+    return Promise.resolve(db);
+  });
+}
+
+module.exports = mockDatabase;
