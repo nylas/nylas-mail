b5bf4e07c4bafd035e7567e035a8853e0d91781a
Evan Morikawa <evan@evanmorikawa.com>
2016-11-23 09:06:56 -0800

    [isomorphic-core] Move files into src folder for isomorphic-core

--- packages/isomorphic-core/index.js
+++ packages/isomorphic-core/index.js
@@ -4,7 +4,7 @@ module.exports = {
     IMAP: 'imap',
   },
   Imap: require('imap'),
-  IMAPConnection: require('./imap-connection'),
-  IMAPErrors: require('./imap-errors'),
-  PromiseUtils: require('./promise-utils'),
+  IMAPConnection: require('src/imap-connection'),
+  IMAPErrors: require('src/imap-errors'),
+  PromiseUtils: require('src/promise-utils'),
 }
