3b7c40a870332460a079d8f15d006deaeb2f5f0b
Juan Tejada <juans.tejada@gmail.com>
2016-11-22 20:25:43 -0800

    [local-sync] Misc fixes from database-connector move

--- packages/local-sync/src/shared/scheduler-utils.js
+++ packages/local-sync/src/shared/scheduler-utils.js
@@ -8,9 +8,8 @@ const HEARTBEAT_EXPIRES = 30; // 2 min in prod?
 
 const CLAIM_DURATION = 10 * 60 * 1000; // 2 hours on prod?
 
-const PromiseUtils = require('./promise-utils')
+const {PromiseUtils, MessageTypes} = require('nylas-core')
 const LocalPubsubConnector = require('./local-pubsub-connector');
-const MessageTypes = require('./message-types')
 
 const forEachAccountList = (forEachCallback) => {
   const client = LocalPubsubConnector.broadcastClient();
