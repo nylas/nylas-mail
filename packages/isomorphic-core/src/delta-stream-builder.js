5ff7a921977a1732a153d5817851a334ffcafba8
Ben Gotow <bengotow@gmail.com>
2016-12-01 18:54:26 -0800

    [*] fix(deltas): 0 as first cursor, deltas from redis are already JSON

--- packages/isomorphic-core/src/delta-stream-builder.js
+++ packages/isomorphic-core/src/delta-stream-builder.js
@@ -3,7 +3,7 @@ const Rx = require('rx')
 const stream = require('stream');
 
 function stringifyTransactions(db, transactions = []) {
-  const transactionJSONs = transactions.map((t) => t.toJSON())
+  const transactionJSONs = transactions.map((t) => (t.toJSON ? t.toJSON() : t))
   transactionJSONs.forEach((t) => { t.cursor = t.id });
 
   const byModel = _.groupBy(transactionJSONs, "object");
@@ -57,7 +57,7 @@ module.exports = {
   buildCursor({databasePromise}) {
     return databasePromise.then(({Transaction}) => {
       return Transaction.findOne({order: [['id', 'DESC']]}).then((t) => {
-        return (t || {}).id;
+        return t ? t.id : 0;
       });
     });
   },
