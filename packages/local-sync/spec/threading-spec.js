269d61171a782713ecfbfd0af9940e428adcfb1c
Ben Gotow <bengotow@gmail.com>
2016-12-05 15:00:21 -0800

    [local-sync] fix(specs): New specs for threading

--- packages/local-sync/spec/threading-spec.js
+++ packages/local-sync/spec/threading-spec.js
@@ -1,55 +1,81 @@
 /* eslint global-require: 0 */
 /* eslint import/no-dynamic-require: 0 */
-// const path = require('path')
-// const BASE_PATH = path.join(__dirname, 'fixtures')
+const path = require('path');
+const detectThread = require('../src/new-message-processor/detect-thread');
+const LocalDatabaseConnector = require('../src/shared/local-database-connector');
 
-xdescribe('threading', function threadingSpecs() {
-  // it('adds the message to the thread', (done) => {
-  //   const {message, reply} = require(`${BASE_PATH}/thread`)
-  //   const accountId = 'a-1'
-  //   const mockDb = {
-  //     Thread: {
-  //       findAll: () => {
-  //         return Promise.resolve([{
-  //           id: 1,
-  //           subject: "Loved your work and interests",
-  //           messages: [message],
-  //         }])
-  //       },
-  //       find: () => {
-  //         return Promise.resolve(null)
-  //       },
-  //       create: (thread) => {
-  //         thread.id = 1
-  //         thread.addMessage = (newMessage) => {
-  //           if (thread.messages) {
-  //             thread.messages.push(newMessage.id)
-  //           } else {
-  //             thread.messages = [newMessage.id]
-  //           }
-  //         }
-  //         return Promise.resolve(thread)
-  //       },
-  //     },
-  //     Message: {
-  //       findAll: () => {
-  //         return Promise.resolve([message, reply])
-  //       },
-  //       find: () => {
-  //         return Promise.resolve(reply)
-  //       },
-  //       create: () => {
-  //         message.setThread = (thread) => {
-  //           message.thread = thread.id
-  //         };
-  //         return Promise.resolve(message);
-  //       },
-  //     },
-  //   }
-  //
-  //   processMessage({db: mockDb, message: reply, accountId}).then((processed) => {
-  //     expect(processed.thread).toBe(1)
-  //     done()
-  //   })
-  // });
+const FIXTURES_PATH = path.join(__dirname, 'fixtures');
+const ACCOUNT_ID = 'test-account-threading';
+
+function messagesFromFixture({Message}, folder, name) {
+  const {A, B} = require(`${FIXTURES_PATH}/Threading/${name}`)
+
+  const msgA = Message.build(A);
+  msgA.folder = folder;
+  msgA.labels = [];
+
+  const msgB = Message.build(B);
+  msgB.folder = folder;
+  msgB.labels = [];
+
+  return {msgA, msgB};
+}
+
+describe('threading', function threadingSpecs() {
+  beforeEach(() => {
+    waitsForPromise({timeout: 1000}, async () => {
+      await LocalDatabaseConnector.ensureAccountDatabase(ACCOUNT_ID);
+      this.db = await LocalDatabaseConnector.forAccount(ACCOUNT_ID);
+      this.folder = await this.db.Folder.create({
+        id: 'test-folder-id',
+        accountId: ACCOUNT_ID,
+        version: 1,
+        name: 'Test Folder',
+        role: null,
+      });
+    });
+  });
+
+  afterEach(() => {
+    LocalDatabaseConnector.destroyAccountDatabase(ACCOUNT_ID)
+  })
+
+  describe("when remote thread ids are present", () => {
+    it('threads emails with the same gthreadid', () => {
+      waitsForPromise(async () => {
+        const {msgA, msgB} = messagesFromFixture(this.db, this.folder, 'remote-thread-id-yes');
+        const threadA = await detectThread({db: this.db, message: msgA});
+        const threadB = await detectThread({db: this.db, message: msgB});
+        expect(threadB.id).toEqual(threadA.id);
+      });
+    });
+
+    it('does not thread other emails', () => {
+      waitsForPromise(async () => {
+        const {msgA, msgB} = messagesFromFixture(this.db, this.folder, 'remote-thread-id-no');
+        const threadA = await detectThread({db: this.db, message: msgA});
+        const threadB = await detectThread({db: this.db, message: msgB});
+        expect(threadB.id).not.toEqual(threadA.id);
+      });
+    });
+  });
+  describe("when subject matching", () => {
+    it('threads emails with the same subject', () => {
+      waitsForPromise(async () => {
+        const {msgA, msgB} = messagesFromFixture(this.db, this.folder, 'subject-matching-yes');
+        const threadA = await detectThread({db: this.db, message: msgA});
+        const threadB = await detectThread({db: this.db, message: msgB});
+        expect(threadB.id).toEqual(threadA.id);
+      });
+    });
+
+    it('does not thread other emails', () => {
+      waitsForPromise(async () => {
+        const {msgA, msgB} = messagesFromFixture(this.db, this.folder, 'subject-matching-no');
+        const threadA = await detectThread({db: this.db, message: msgA});
+        const threadB = await detectThread({db: this.db, message: msgB});
+        expect(threadB.id).not.toEqual(threadA.id);
+      });
+    });
+  });
 });
