7e595487437a73e986444ed27210a606f91f62cf
Ben Gotow <bengotow@gmail.com>
2016-12-05 16:07:46 -0800

    [local-sync] feat(specs): Add basic tests for folder sync

---
+++ packages/local-sync/spec/imap/fetch-folder-list-spec.js
@@ -0,0 +1,45 @@
+
+const FetchFolderList = require('../../src/local-sync-worker/imap/fetch-folder-list');
+const LocalDatabaseConnector = require('../../src/shared/local-database-connector');
+const {forEachJSONFixture, ACCOUNT_ID, silentLogger} = require('../helpers');
+
+describe("FetchFolderList", function FetchFolderListSpecs() {
+  beforeEach(() => {
+    waitsForPromise(async () => {
+      await LocalDatabaseConnector.ensureAccountDatabase(ACCOUNT_ID);
+      this.db = await LocalDatabaseConnector.forAccount(ACCOUNT_ID);
+
+      this.stubImapBoxes = null;
+      this.imap = {
+        getBoxes: () => {
+          return Promise.resolve(this.stubImapBoxes);
+        },
+      };
+    });
+  });
+
+  afterEach(() => {
+    LocalDatabaseConnector.destroyAccountDatabase(ACCOUNT_ID)
+  })
+
+  describe("initial syncing", () => {
+    forEachJSONFixture('FetchFolderList', (filename, json) => {
+      it(`should create folders and labels correctly for boxes (${filename})`, () => {
+        waitsForPromise(async () => {
+          const {boxes, expectedFolders, expectedLabels} = json;
+          const provider = filename.split('-')[0];
+          this.stubImapBoxes = boxes;
+
+          const task = new FetchFolderList(provider, silentLogger);
+          await task.run(this.db, this.imap);
+
+          const folders = await this.db.Folder.findAll();
+          expect(folders.map((f) => { return {name: f.name, role: f.role} })).toEqual(expectedFolders);
+
+          const labels = await this.db.Label.findAll();
+          expect(labels.map(f => { return {name: f.name, role: f.role} })).toEqual(expectedLabels);
+        });
+      });
+    });
+  });
+});
--- packages/local-sync/spec/message-factory-spec.js
+++ packages/local-sync/spec/message-factory-spec.js
@@ -1,35 +1,31 @@
-const path = require('path');
-const fs = require('fs');
 const LocalDatabaseConnector = require('../src/shared/local-database-connector');
 const {parseFromImap} = require('../src/shared/message-factory');
-
-const FIXTURES_PATH = path.join(__dirname, 'fixtures')
+const {forEachJSONFixture, ACCOUNT_ID} = require('./helpers');
 
 describe('MessageFactory', function MessageFactorySpecs() {
   beforeEach(() => {
     waitsForPromise(async () => {
-      const accountId = 'test-account-id';
-      await LocalDatabaseConnector.ensureAccountDatabase(accountId);
-      const db = await LocalDatabaseConnector.forAccount(accountId);
+      await LocalDatabaseConnector.ensureAccountDatabase(ACCOUNT_ID);
+      const db = await LocalDatabaseConnector.forAccount(ACCOUNT_ID);
       const folder = await db.Folder.create({
         id: 'test-folder-id',
-        accountId: accountId,
+        accountId: ACCOUNT_ID,
         version: 1,
         name: 'Test Folder',
         role: null,
       });
-      this.options = { accountId, db, folder };
+      this.options = { accountId: ACCOUNT_ID, db, folder };
     })
   })
 
-  describe("parseFromImap", () => {
-    const fixturesDir = path.join(FIXTURES_PATH, 'MessageFactory', 'parseFromImap');
-    const filenames = fs.readdirSync(fixturesDir).filter(f => f.endsWith('.json'));
+  afterEach(() => {
+    LocalDatabaseConnector.destroyAccountDatabase(ACCOUNT_ID)
+  })
 
-    filenames.forEach((filename) => {
+  describe("parseFromImap", () => {
+    forEachJSONFixture('MessageFactory/parseFromImap', (filename, json) => {
       it(`should correctly build message properties for ${filename}`, () => {
-        const inJSON = JSON.parse(fs.readFileSync(path.join(fixturesDir, filename)));
-        const {imapMessage, desiredParts, result} = inJSON;
+        const {imapMessage, desiredParts, result} = json;
 
         waitsForPromise(async () => {
           const actual = await parseFromImap(imapMessage, desiredParts, this.options);
