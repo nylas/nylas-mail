57682a0ff596fc0e12eebeb2a91fe9e6b89fda78
Christine Spang <spang@nylas.com>
2016-12-16 14:17:15 -0800

    [local-sync] Fix parseFromImap specs
    Lots has changed for the better! Tests work again also.

--- packages/local-sync/spec/message-factory-spec.js
+++ packages/local-sync/spec/message-factory-spec.js
@@ -2,7 +2,7 @@ const LocalDatabaseConnector = require('../src/shared/local-database-connector')
 const {parseFromImap, extractSnippet} = require('../src/shared/message-factory');
 const {forEachJSONFixture, forEachHTMLAndTXTFixture, ACCOUNT_ID} = require('./helpers');
 
-xdescribe('MessageFactory', function MessageFactorySpecs() {
+fdescribe('MessageFactory', function MessageFactorySpecs() {
   beforeEach(() => {
     waitsForPromise(async () => {
       await LocalDatabaseConnector.ensureAccountDatabase(ACCOUNT_ID);
