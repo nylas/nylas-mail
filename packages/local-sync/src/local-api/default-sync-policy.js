79696237030ced3701a2a0d12fab8d337ea30155
Evan Morikawa <evan@evanmorikawa.com>
2017-01-13 12:00:45 -0800

    [local-sync] fix attribute scanning in sync worker
    Summary:
    Fixes https://phab.nylas.com/T7435
    
    The old deepScan (now `scanForAttributeChanges`) and shallowScan (now
    `fetchLatestAttributeChanges`) had some fatal flaws.
    
    If you deep scanned it would attempt to load the message attributes of all
    messages ever and cause very bad memory leaks.
    
    Also, if you left a mailbox running for a long time, there was a query
    that would eventually run `Message.findAll` and, even though it was just
    returning the headers, would still run insanely expensive operations
    
    This fixes (and renames) these issues.
    
    Test Plan: manual
    
    Reviewers: spang, halla, juan
    
    Reviewed By: juan
    
    Differential Revision: https://phab.nylas.com/D3657

--- packages/local-sync/src/local-api/default-sync-policy.js
+++ packages/local-sync/src/local-api/default-sync-policy.js
@@ -3,8 +3,5 @@ const DefaultSyncPolicy = {
     active: 10 * 1000,
     inactive: 5 * 60 * 1000,
   },
-  folderSyncOptions: {
-    deepFolderScan: 10 * 60 * 1000,
-  },
 }
 module.exports = DefaultSyncPolicy;
