0dfe588213c5dc21bae377cb59bb770162018001
Christine Spang <spang@nylas.com>
2017-02-01 07:31:18 -0800

    [local-api] :art: Remove unused message and file GET routes
    Summary:
    This code is dead and has confused me grepping around the codebase before.
    
    If for some unexpected reason we need these routes back in the future, we
    can always extract them from version control. For now the routes we aren't
    using are a distraction.
    
    Test Plan: been using Nylas Mail with this local patch all week
    
    Reviewers: evan, halla, juan
    
    Reviewed By: juan
    
    Differential Revision: https://phab.nylas.com/D3833

--- packages/local-sync/src/local-api/routes/files.js
+++ packages/local-sync/src/local-api/routes/files.js
@@ -1,77 +1,7 @@
 const Joi = require('joi');
-const Serialization = require('../serialization');
 
 
 module.exports = (server) => {
-  server.route({
-    method: 'GET',
-    path: '/files',
-    config: {
-      description: 'Returns an array of file metadata.',
-      notes: 'Notes go here',
-      tags: ['files'],
-      validate: {
-        query: {
-          limit: Joi.number().integer().min(1).max(2000).default(100),
-          offset: Joi.number().integer().min(0).default(0),
-        },
-      },
-      response: {
-        schema: Joi.array().items(
-          Serialization.jsonSchema('File')
-        ),
-      },
-    },
-    handler: (request, reply) => {
-      request.getAccountDatabase().then((db) => {
-        const {File} = db;
-        File.findAll({
-          limit: request.query.limit,
-          offset: request.query.offset,
-        }).then((files) => {
-          reply(Serialization.jsonStringify(files));
-        })
-      })
-    },
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/files/{id}',
-    config: {
-      description: 'Returns file with specified id.',
-      notes: 'Notes go here',
-      tags: ['files'],
-      validate: {
-        params: {
-          id: Joi.string(),
-        },
-      },
-      response: {
-        schema: Joi.alternatives().try(
-          Serialization.jsonSchema('File'),
-          Joi.string()
-        ),
-      },
-    },
-    handler: (request, reply) => {
-      request.getAccountDatabase().then(({File}) => {
-        const {params: {id}} = request
-
-        File.findOne({where: {id}}).then((file) => {
-          if (!file) {
-            return reply.notFound(`File ${id} not found`)
-          }
-          return reply(Serialization.jsonStringify(file));
-        })
-        .catch((err) => {
-          request.logger.error(err, 'Error fetching file')
-          reply(err)
-        })
-      })
-    },
-  })
-
   server.route({
     method: 'GET',
     path: '/files/{id}/download',
