6a17073fe5616ecfd95b87f3bf8432988dafaf04
Mark Hahnenberg <mark@nylas.com>
2017-03-09 14:03:29 -0800

    [client-sync] Refactor file download IMAP pool use
    Summary:
    This is in preparation for removing the ability of the IMAP pool to
    automatically handle timeout errors.
    
    Test Plan: Run locally, verify file download still works
    
    Reviewers: juan
    
    Reviewed By: juan
    
    Differential Revision: https://phab.nylas.com/D4167

--- packages/client-sync/src/models/file.js
+++ packages/client-sync/src/models/file.js
@@ -29,52 +29,58 @@ module.exports = (sequelize, Sequelize) => {
       async fetch({account, db, logger}) {
         const message = await this.getMessage()
         const folder = await message.getFolder()
+
         let numTimeoutErrors = 0;
         let result = null;
+
+        const onConnected = async ([connection], done) => {
+          const imapBox = await connection.openBox(folder.name)
+          const stream = await imapBox.fetchMessageStream(message.folderImapUID, {
+            fetchOptions: {
+              bodies: this.partId ? [this.partId] : [],
+              struct: true,
+            },
+            onFetchComplete() {
+              done();
+            },
+          });
+
+          if (!stream) {
+            throw new Error(`Unable to fetch binary data for File ${this.id}`)
+          }
+
+          if (/quoted-printable/i.test(this.encoding)) {
+            result = stream.pipe(new QuotedPrintableStreamDecoder({charset: this.charset}));
+            return true;
+          } else if (/base64/i.test(this.encoding)) {
+            result = stream.pipe(base64.decode());
+            return true;
+          }
+
+          // If there is no encoding, or the encoding is something like
+          // '7bit', '8bit', or 'binary', just return the raw stream. This
+          // stream will be written directly to disk. It's then up to the
+          // user's computer to decide how to interpret the bytes we've
+          // dumped to disk.
+          result = stream;
+          return true;
+        };
+
+        const onTimeout = (socketTimeout) => {
+          numTimeoutErrors += 1;
+          Actions.recordUserEvent('Timeout error downloading file', {
+            accountId: account.id,
+            provider: account.provider,
+            socketTimeout,
+            numTimeoutErrors,
+          });
+        };
+
         await IMAPConnectionPool.withConnectionsForAccount(account, {
           desiredCount: 1,
           logger,
-          onConnected: async ([connection], done) => {
-            const imapBox = await connection.openBox(folder.name)
-            const stream = await imapBox.fetchMessageStream(message.folderImapUID, {
-              fetchOptions: {
-                bodies: this.partId ? [this.partId] : [],
-                struct: true,
-              },
-              onFetchComplete() {
-                done();
-              },
-            });
-
-            if (!stream) {
-              throw new Error(`Unable to fetch binary data for File ${this.id}`)
-            }
-
-            if (/quoted-printable/i.test(this.encoding)) {
-              result = stream.pipe(new QuotedPrintableStreamDecoder({charset: this.charset}));
-              return true;
-            } else if (/base64/i.test(this.encoding)) {
-              result = stream.pipe(base64.decode());
-              return true;
-            }
-
-            // If there is no encoding, or the encoding is something like
-            // '7bit', '8bit', or 'binary', just return the raw stream. This
-            // stream will be written directly to disk. It's then up to the
-            // user's computer to decide how to interpret the bytes we've
-            // dumped to disk.
-            result = stream;
-            return true;
-          },
-          onTimeout: (socketTimeout) => {
-            numTimeoutErrors += 1;
-            Actions.recordUserEvent('Timeout error downloading file', {
-              accountId: account.id,
-              provider: account.provider,
-              socketTimeout,
-              numTimeoutErrors,
-            });
-          },
+          onConnected,
+          onTimeout,
         });
         return result;
       },
