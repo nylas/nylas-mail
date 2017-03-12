a25ad1e7ad78a2d0baa015d1f3d6c391e5c5372a
Christine Spang <spang@nylas.com>
2017-03-11 18:47:22 -0800

    [dev] On Linux, don't run redis if already running
    Summary:
    Dunno what the default behaviour is on Ubuntu, but on my system (Debian
    sid) when I have the redis-server package installed it starts up
    automatically on system boot. In this case, placate pm2 by having the
    launch script sleep forever.
    
    Otherwise, the 'redis' process startup will fail repeatedly and spam the
    console.
    
    Test Plan: manual
    
    Reviewers: halla
    
    Reviewed By: halla
    
    Differential Revision: https://phab.nylas.com/D4203

--- packages/cloud-core/scripts/run-redis.sh
+++ packages/cloud-core/scripts/run-redis.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-# Redis is one of those servers which by default
-# accept connections from everywhere. Luckily,
-# homebrew and presumably debian come with sane
-# defaults. However, they're located in different
-# directories.
+# Redis is one of those servers which by default accept connections from
+# everywhere. Luckily, homebrew and presumably debian come with sane defaults.
+# However, they're located in different directories.
 if [[ $(uname) = 'Darwin' ]]; then
     echo "Running redis from Homebrew..."
     redis-server /usr/local/etc/redis.conf
 fi
 
 if [[ $(uname) = 'Linux' ]]; then
-    echo "Running redis"
-    redis-server /etc/redis/redis.conf
+    # redis-server package may have redis running by default; don't crash if so
+    pgrep -lf redis-server
+    if [ $? -ne 0 ]; then
+        echo "Running redis"
+        redis-server /etc/redis/redis.conf
+    else
+        echo "Redis already running"
+        sleep infinity
+    fi
 fi
