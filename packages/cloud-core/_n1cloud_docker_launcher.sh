e80f893fbc3b8f1a75884b5b880817e5333ee5ed
Jeremy Price <jeremy@nylas.com>
2017-03-18 14:06:33 -0700

    [n1cloud][executiveintro][docker] updating docker
    launch file to be more flexible. We now have a case statement
    to take the AWS_SERVICE_NAME we pass in via Dockerfile and run
    a definitive list of commands, rather than having to key off the
    interpreted command line directly. Makes it lots easier to add/change
    commands
    
    Added symlinks to the pm2-prod-api/workers.yml files for clearer naming
    within the script, ie pm2-prod-n1cloud-api/workers.yml.  Anticipating
    pm2-prod-ei-{task}.yml files to come. Didn't just rename them on the off
    chance something else depends on them
    
    Also added some minimal logging

--- packages/cloud-core/_n1cloud_docker_launcher.sh
+++ packages/cloud-core/_n1cloud_docker_launcher.sh
@@ -1,5 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 # This is run from the DOCKERFILE
 # The cwd context is where the DOCKERFILE is at the root of /nylas-mail-all
-./node_modules/pm2/bin/pm2 start packages/cloud-core/pm2-prod-$1.yml
-./node_modules/pm2/bin/pm2 logs --raw
+
+[ -z "$1" ] && echo '{"docker_startup": "FAILED", "error": "must include an AWS_SERVICE_NAME as arg1"}' && exit 1
+AWS_SERVICE_NAME="$1"
+
+case $AWS_SERVICE_NAME in 
+    api|n1cloud-api)
+        APP="n1cloud-api"
+        echo '{"docker_startup": "'$APP'"}'
+        ./node_modules/pm2/bin/pm2 start packages/cloud-core/pm2-prod-n1cloud-api.yml
+        ./node_modules/pm2/bin/pm2 logs --raw
+        ;;
+    workers|worker|n1cloud-worker|n1cloud-workers)
+        APP="n1cloud-workers"
+        echo '{"docker_startup": "'$APP'"}'
+        ./node_modules/pm2/bin/pm2 start packages/cloud-core/pm2-prod-n1cloud-workers.yml
+        ./node_modules/pm2/bin/pm2 logs --raw
+        ;;
+    ei)
+        APP="executiveintro"
+        echo '{"docker_startup": "'$APP'"}'
+        ## Uncomment these lines and update as necessary.
+        #./node_modules/pm3/bin/pm2 start packages/cloud-core/pm2-prod-ei-frontend.yml
+        #./node_modules/pm3/bin/pm2 start packages/cloud-core/pm2-prod-ei-backend.yml
+        ./node_modules/pm2/bin/pm2 logs --raw
+        ;;
+     *)
+        echo '{"docker_startup": "FAILED", "error": "unknown AWS_SERVICE_NAME name '$AWS_SERVICE_NAME'"}'
+        exit 2
+        ;;
+esac
