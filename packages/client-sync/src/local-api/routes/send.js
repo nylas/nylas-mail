3e44c96b4840eb63267fa85857ed30d8cb434d79
Halla Moore <halla@nylas.com>
2017-04-13 17:39:14 -0700

    [client-sync] Remove deprecated /send routes
    Summary: We use Actions instead of http requests for these now.
    
    Test Plan: manual
    
    Reviewers: mark, evan, juan
    
    Reviewed By: evan, juan
    
    Differential Revision: https://phab.nylas.com/D4438

--- packages/client-sync/src/local-api/routes/send.js
+++ packages/client-sync/src/local-api/routes/send.js
@@ -18,39 +18,6 @@ const {createAndReplyWithSyncbackRequest} = require('../route-helpers');
 // const recipientList = Joi.array().items(recipient);
 
 module.exports = (server) => {
-  server.route({
-    method: 'POST',
-    path: '/send',
-    config: {
-    },
-    async handler(request, reply) {
-      createAndReplyWithSyncbackRequest(request, reply, {
-        type: "SendMessage",
-        props: {
-          messagePayload: request.payload,
-        },
-      })
-    },
-  });
-
-  // Initiates a multi-send session by creating a new multi-send draft.
-  server.route({
-    method: 'POST',
-    path: '/send-per-recipient',
-    config: {
-    },
-    async handler(request, reply) {
-      createAndReplyWithSyncbackRequest(request, reply, {
-        type: "SendMessagePerRecipient",
-        props: {
-          messagePayload: request.payload.message,
-          usesOpenTracking: request.payload.uses_open_tracking,
-          usesLinkTracking: request.payload.uses_link_tracking,
-        },
-      })
-    },
-  });
-
   // Closes out a multi-send session by marking the sending draft as sent
   // and moving it to the user's Sent folder.
   server.route({
