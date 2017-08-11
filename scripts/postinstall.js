29c89acb60c90d8255c3f90dce0c3720f22d7eb7
Ben Gotow <ben@foundry376.com>
2017-08-10 22:47:06 -0700

    Enable app/node_modules caching, make postinstall check its Electron version

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -2,12 +2,15 @@
 /* eslint global-require: 0 */
 /* eslint quote-props: 0 */
 const path = require('path');
+const fs = require('fs');
+const rimraf = require('rimraf');
 const safeExec = require('./utils/child-process-wrapper.js').safeExec;
 
+const npmElectronTarget = require('../app/package.json').dependencies.electron;
 const npmEnvs = {
   system: process.env,
   electron: Object.assign({}, process.env, {
-    'npm_config_target': require('../app/package.json').dependencies.electron,
+    'npm_config_target': npmElectronTarget,
     'npm_config_arch': process.arch,
     'npm_config_target_arch': process.arch,
     'npm_config_disturl': 'https://atom.io/download/atom-shell',
@@ -31,5 +34,26 @@ function npm(cmd, options) {
   });
 }
 
-npm('install', {cwd: './app', env: 'electron'})
-.then(() => npm('dedupe', {cwd: './app', env: 'electron'}))
+async function go() {
+  // For speed, we cache app/node_modules. However, we need to
+  // be sure to do a full rebuild of native node modules when the
+  // Electron version changes. To do this we check a marker file.
+  const cacheVersionPath = './app/node_modules/.postinstall-target-version';
+  const cacheElectronTarget = fs.existsSync(cacheVersionPath) && fs.readFileSync(cacheVersionPath).toString();
+
+  if (cacheElectronTarget !== npmElectronTarget) {
+    console.log(`\n-- Clearing app/node_modules --`)
+    rimraf.sync(path.resolve(__dirname, '..', 'app', 'node_modules'));
+  }
+
+  // run `npm install` in ./app with Electron NPM config
+  await npm('install', {cwd: './app', env: 'electron'});
+
+  // run `npm dedupe` in ./app with Electron NPM config
+  await npm('dedupe', {cwd: './app', env: 'electron'});
+
+  // write the marker with the electron version
+  fs.writeFileSync(cacheVersionPath, npmElectronTarget);
+}
+
+go();
